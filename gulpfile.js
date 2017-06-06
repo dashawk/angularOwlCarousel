a9f5976e556e0cc30b8e847aded4a6125d2c1df6
Jason P <jmpanugaling@gmail.com>
2017-06-06 11:47:06 +0800

    updated gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 var gulp = require('gulp');
+var pump = require('pump');
 var $ = require('gulp-load-plugins')({ lazy: true });
 
-gulp.task('minify', function () {
-	return gulp
-		.src('./src/angular-carousel.js')
-		.pipe();
-});
-
 gulp.task('default', function () {
-
+	pump([
+		gulp.src('./src/angular-carousel.js'),
+		$.uglify(),
+		$.ngAnnotate({ add: true }),
+		$.rename({ suffix: '.min'}),
+		gulp.dest('./dist')
+	]);
 });
